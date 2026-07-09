b2d936be68a641bd3b77c9b6ce682407e12e0b0e
Arman Rizvi <210411543+Alpha241026@users.noreply.github.com>
2026-07-09 15:39:16 +0530

    updated goroutine program

--- 8ConcurrencyMastery/1Goroutine/Goroutine.go
+++ 8ConcurrencyMastery/1Goroutine/Goroutine.go
@@ -5,19 +5,24 @@ import (
 	"time"
 )
 
+// function executed by each goroutine
 func sayHello(message string, delay time.Duration) {
-	time.Sleep(delay)
+	time.Sleep(delay) //simulating some work
 	fmt.Println("sayHello", message)
 }
 
 func main() {
-	fmt.Println("Hello from Main() Goroutine")
+	fmt.Println("Hello from Main() Goroutine") //every Go program starts with one goroutine: the main goroutine
 
+	//independent goroutines
 	go sayHello("Hello World 1", time.Second)
 	go sayHello("Hello World 2", time.Second)
 	go sayHello("Hello World from 2 seconds", 2*time.Second)
 	go sayHello("Hello World from 3 seconds", 3*time.Second)
 
+	//main continues immediately without waiting
 	fmt.Println("Last message from Main() Goroutine")
+
+	//keeping program alive temporarily...since all other goroutines stop when main exits
 	time.Sleep(2 * time.Second)
 }
